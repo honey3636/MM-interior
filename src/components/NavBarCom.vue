439603189bfb4e162d14007a4f28c15688245d78
honey3636 <hhhhoney2003@gmail.com>
2024-07-26 14:33:04 -0800

    first commit

---
+++ src/components/NavBarCom.vue
@@ -0,0 +1,20 @@
+<template>
+<h2>navBarCom</h2>
+</template>
+
+<!-- script section -->
+<script>
+
+export default{
+    name:"NavBarCom",
+    data(){
+        return
+    }
+
+}
+</script>
+
+<!-- style section -->
+<style>
+
+</style>
